d7e13044661b0c72e9e7231a925744f3becd835a
Chris <chriskang0917@gmail.com>
2023-06-22 16:29:19 +0800

    Add following feature: - getStaticPaths and merge with getStaticProps - API routes for handling Requests

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -4,15 +4,26 @@ const DetailsPage = () => {
   return <MeetupDetail />;
 };
 
-// export async function getStaticProps(context) {
-//   const meetupId = context.params.meetupId;
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      { params: { meetupId: "1" } },
+      { params: { meetupId: "2" } },
+      { params: { meetupId: "3" } },
+    ],
+  };
+}
 
-//   return {
-//     props: {
-//       meetupData: {},
-//     },
-//     revalidate: 60,
-//   };
-// }
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {},
+    },
+    revalidate: 60,
+  };
+}
 
 export default DetailsPage;
